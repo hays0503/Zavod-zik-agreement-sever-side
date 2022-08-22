303b577f83f9b89e0aae4aae726be46b635814c5
hays0503 <cthtufgbv@mail.ru>
2022-08-22 14:42:31 +0600

    фикс прикреплённых файлов (по поручению)

--- server.js
+++ server.js
@@ -199,6 +199,25 @@ app.post('/api/files', (req, res, next) => {
     });
 });
 
+
+//file downloads from browser(Скачивания файлов из таблиц по поручениям)
+app.post('/api/tasks_files', (req, res, next) => {
+    console.log('req.body files',req.body)
+    filePgClient.query( `SELECT id, filename, data_file FROM document_tasks_files WHERE id = ${req.body.item}`, (err, result) => {
+        if (err) {
+            return console.error('error head reply pg query:', err);
+        }
+        //console.log("result.rows[0].id",result.rows[0].id)
+        let temp = {filename:result.rows[0].filename, type:'file_open', data:`${result.rows[0].data_file.substr(result.rows[0].data_file.lastIndexOf(',')+1)}`}
+        sdServerWSConfig.connect.clients.forEach(async function each(ws) {
+            console.log('filename', result.rows[0].filename, req.body.user, ws.id)
+            if (req.body.user==ws.id)
+            ws.send(JSON.stringify(temp))
+        });
+    });
+});
+
+
 /*app.post('/api/notifications', (req, res, next) => {
     sendPopEmail(req.body.address)
     let temp = {type:'notification'}
@@ -296,6 +315,20 @@ app.post("/get-file", async (req, res) => {
     console.log(req.body)
     let id = req.body.id
     let result = await client.query(`SELECT * FROM document_files WHERE id = ${id}`)
+    console.log(result.rows[0]);
+
+    res.json({ result: result.rows[0] })
+})
+
+app.post("/get-tasks-file", async (req, res) => {
+    const { writeFile } = require('fs');
+    const { promisify } = require('util');
+    const writeFilePromise = promisify(writeFile);
+
+    let client = require("./config/pgConfig")
+    console.log(req.body)
+    let id = req.body.id
+    let result = await client.query(`SELECT * FROM document_tasks_files WHERE id = ${id}`)
     console.log(result.rows[0]);
 
     res.json({ result: result.rows[0] })
