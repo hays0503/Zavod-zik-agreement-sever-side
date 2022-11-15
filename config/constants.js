33264a2a8ee7527f8b40da20e6808e2e65eac3df
RyndychRD <r.roman1234@yandex.ru>
2022-11-15 17:54:35 +0600

    Сделал отсылку уведомлений по email отключаемой. Теперь отсылаются если в sd-config.ini стоит isSendEmail=1

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
-const ini = require('ini');
-const process = require('process');
+const ini = require("ini");
+const process = require("process");
 //console.log(path.dirname(process.execPath));
-let config = ini.parse(fs.readFileSync(path.dirname(process.execPath)+'\\sd-config.ini', 'utf-8'));
+let config = ini.parse(
+  fs.readFileSync(path.dirname(process.execPath) + "\\sd-config.ini", "utf-8")
+);
 fs.close;
 
 //получение хоста из конф файла ura
@@ -11,33 +13,33 @@ fs.close;
 //let getIp=JSON.parse(fileContent);
 
 const constants = {
-    debug: false,
-    port: 8445,
-    host: config.HardSettings.Host,
-    database: {
-        user: 'postgres',
-        password: config.HardSettings.Password,//Buffer.from("em9pdGliMjNHdmVyZGU=", 'base64').toString('ascii'),
-        host: '127.0.0.1',
-        port: config.HardSettings.DbPort,
-        database: 'agreement'
-    },
-    graphql: {
-        path: '/graphql'
-    },
-    guard: {
-        port: 9988,
-        path: '/sd/d037b3b1d204fda81bca6cf1b3cceb50'
-    },
-    wss: {
-        port: 9996,
-    },
-    session: {
-        table: 'sessions',
-        secret: 'a4454fafd08f4b9d411167896035e682a4454fafd08f4b9d411167896035e682'
-    },
-	hardware: {
-		useMacroscop: config.HardSettings.UseMacroscop
-	}
-}
+  debug: false,
+  port: 8445,
+  host: config.HardSettings.Host,
+  database: {
+    user: "postgres",
+    password: config.HardSettings.Password, //Buffer.from("em9pdGliMjNHdmVyZGU=", 'base64').toString('ascii'),
+    host: "127.0.0.1",
+    port: config.HardSettings.DbPort,
+    database: "agreement",
+  },
+  graphql: {
+    path: "/graphql",
+  },
+  guard: {
+    port: 9988,
+    path: "/sd/d037b3b1d204fda81bca6cf1b3cceb50",
+  },
+  wss: {
+    port: 9996,
+  },
+  session: {
+    table: "sessions",
+    secret: "a4454fafd08f4b9d411167896035e682a4454fafd08f4b9d411167896035e682",
+  },
+  hardware: {
+    useMacroscop: config.HardSettings.UseMacroscop,
+  },
+};
 
 module.exports = constants;
